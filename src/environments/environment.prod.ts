455f1886351d1c3b93bea9669bcef23a29eb7846
leost3 <leost3@gmail.com>
2019-11-07 11:09:56 -0500

    dropdown meny

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDUGz6cVvfyNME6vABlpUngU4rUyJ7Ih18',
+    authDomain: 'organic-shop-28b35.firebaseapp.com',
+    databaseURL: 'https://organic-shop-28b35.firebaseio.com',
+    projectId: 'organic-shop-28b35',
+    storageBucket: 'organic-shop-28b35.appspot.com',
+    messagingSenderId: '303415639710',
+    appId: '1:303415639710:web:5febbc6737d470f244058f',
+    measurementId: 'G-V8463QSTSK'
+  }
 };
